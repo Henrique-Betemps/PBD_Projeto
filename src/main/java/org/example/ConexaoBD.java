e86d88f61b56c2fde947e97526491b6ce802905c
Henrique Betemps <henrique.betempsbarbosa@gmail.com>
2026-07-08 19:20:00 -0300

    Commit final para o Trabalho de PBD

--- src/main/java/org/example/ConexaoBD.java
+++ src/main/java/org/example/ConexaoBD.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class ConexaoBD {
     private static final String URL = "jdbc:postgresql://localhost:5432/ufpel_resolve";
-    private static final String USER = "usuario";
-    private static final String PASSWORD = "usuario";
+    private static final String USER = "seu_usuario_aqui";
+    private static final String PASSWORD = "sua_senha_aqui";
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
